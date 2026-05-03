a67e67a3221c4998008c84414819f5544642c8b8
cubinez85 <cubinez85@mail.ru>
2026-05-03 19:58:24 +0300

    contractor_checker/app/admin/__init__.py

--- contractor_checker/app/admin/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('admin', __name__, url_prefix='/admin')
-
-# Этот файл нужен только для blueprint, 
-# регистрация ModelView будет происходить в app/__init__.py после создания admin
